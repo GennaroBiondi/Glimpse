834213bf77cdd234cba888a9404a47ea912d4fd3
Gennaro_Biondi <biondigenny31@gmail.com>
2026-05-02 15:48:23 +0200

    Update app info to use string_view instead of string

--- src/app/info.hpp
+++ src/app/info.hpp
@@ -1,10 +1,10 @@
 #ifndef APP_INFO_HPP
 #define APP_INFO_HPP
 
-#include <string>
+#include <string_view>
 
 namespace App {
-constexpr std::string PROGRAM_VERSION = "1.0.0";
+constexpr std::string_view PROGRAM_VERSION = "1.0.0";
 }
 
 #endif
