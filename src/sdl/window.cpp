67400b8ee071c217742af51fbbdbc9f214c01ce3
Gennaro_Biondi <biondigenny31@gmail.com>
2026-05-02 15:50:06 +0200

    Make Window::display_window use SDL_Init()

--- src/sdl/window.cpp
+++ src/sdl/window.cpp
@@ -8,11 +8,14 @@ Window::Window(const Vector2 &vec2, const Dim2 &dim, std::string_view name)
     : position(vec2), dimension(dim), name(name) {}
 
 void Window::display_window() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        throw std::runtime_error(std::string("Failed to init SDL: ") +
+                                 SDL_GetError());
     this->window =
         SDL_CreateWindow(this->name.c_str(), this->position.x, this->position.y,
                          this->dimension.width, this->dimension.height, 0);
 
-    if (this->window) {
+    if (!this->window) {
         throw std::runtime_error(std::string("Failed to open window. Error: ") +
                                  SDL_GetError());
     }
