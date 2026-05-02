32cf871a3b6f0a1917e16118bdb6199b5752cdad
Gennaro_Biondi <biondigenny31@gmail.com>
2026-05-02 15:49:35 +0200

    Make the ImageInfo struct also hold the image path in the filesystem

--- src/image/image_info.cpp
+++ src/image/image_info.cpp
@@ -1,13 +1,14 @@
 #include "image_info.hpp"
 #include "image_format.hpp"
+#include <filesystem>
 #include <iostream>
 
 ImageInfo::ImageInfo(const Dim2 &d, const ImageFormat::Format f,
-                     std::string_view name)
-    : dimension(d), format(f), name(name) {}
+                     const std::filesystem::path &path, std::string_view name)
+    : dimension(d), format(f), path(path), name(name) {}
 
 ImageInfo::ImageInfo()
-    : dimension{}, format(ImageFormat::Format::UNKNOWN), name{} {}
+    : dimension{}, format(ImageFormat::Format::UNKNOWN), path{}, name{} {}
 
 void ImageInfo::output_info() {
     std::cout << this->name << " info:\n";
@@ -15,4 +16,5 @@ void ImageInfo::output_info() {
               << this->dimension.height << "\n";
     std::cout << "- format:     " << ImageFormat::format_to_string(this->format)
               << "\n";
+    std::cout << "- path:       " << this->path << "\n";
 }
--- src/image/image_info.hpp
+++ src/image/image_info.hpp
@@ -3,16 +3,18 @@
 
 #include "image_format.hpp"
 #include "other/math2d.hpp"
+#include <filesystem>
 #include <string>
 #include <string_view>
 
 struct ImageInfo {
     Dim2 dimension;
+    std::filesystem::path path;
     ImageFormat::Format format;
     std::string name;
 
     ImageInfo(const Dim2 &d, const ImageFormat::Format f,
-              std::string_view name);
+              const std::filesystem::path &p, std::string_view name);
     ImageInfo();
 
     void output_info();
